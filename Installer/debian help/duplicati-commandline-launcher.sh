691696a57765cfbbc616061b108c5ad1114d7471
boredman <boredman@boredomprojects.net>
2014-01-25 10:02:23 +0100

    Installation files

--- Installer/debian help/duplicati-commandline-launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH="/usr/lib/duplicati${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-export MONO_PATH=$MONO_PATH:/usr/lib/duplicati
-
-EXE_FILE=/usr/lib/duplicati/Duplicati.CommandLine.exe
-APP_NAME=Duplicati.CommandLine
-
-exec -a "$APP_NAME" mono "$EXE_FILE" "$@"
--- Installer/debian help/duplicati-launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH="/usr/lib/duplicati${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-export MONO_PATH=$MONO_PATH:/usr/lib/duplicati
-
-EXE_FILE=/usr/lib/duplicati/Duplicati.exe
-APP_NAME=Duplicati
-
-exec -a "$APP_NAME" mono "$EXE_FILE" "$@"
